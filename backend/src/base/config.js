9b2da5e66b0d82393bbb568d16410c60f3860f34
Tushar Singh <ts360523@gmail.com>
2024-01-07 04:22:19 +0530

    Enhancement: Config changes for stores and products.

--- backend/src/base/config.js
+++ backend/src/base/config.js
@@ -3,6 +3,16 @@ const configObect = {
         source: "users",
         defaultSort: "name DESC",
         idProperty: "user_id"
+    },
+    STORES: {
+        source: "stores",
+        defaultSort: "storeName DESC",
+        idProperty: "store_id"
+    },
+    PRODUCTS: {
+        source: "products",
+        defaultSort: "productName DESC",
+        idProperty: "product_id"
     }
 }
 
