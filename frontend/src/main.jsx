8b8b871f2d865c3e0032983292f2c8e7634d164d
Tushar Singh <ts360523@gmail.com>
2024-01-07 14:50:54 +0530

    Enhancement: Side bar, top bar and basic home page.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Login from "./pages/Login/Login";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./utils/store";
+import Login from "./pages/Login/Login";
+import Home from './pages/Home';
+import SideBar from './components/SideBar';
+import "./index.css";
+
+const isAuthenticated = () => {
+  return true;
+};
+
+const ProtectedRoute = ({ element }) => {
+  if (isAuthenticated()) {
+    return (
+        <div>
+            <SideBar element={element}/>
+        </div>
+    );
+  } else {
+    return <Navigate to="/" />;
+  }
+};
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Login />,
     },
+    {
+    path: '/home',
+    element: (
+      <ProtectedRoute
+        element={Home}
+      />
+    ),
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
