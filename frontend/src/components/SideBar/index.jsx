3ba3a6c4670f9b9e5074cc5875051840b893ace9
Tushar Singh <ts360523@gmail.com>
2024-01-08 10:10:53 +0530

    Enhancement: Side bar changes

--- frontend/src/components/SideBar/index.jsx
+++ frontend/src/components/SideBar/index.jsx
@@ -1,184 +1,191 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Icon } from '@mui/material';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import Link from "@mui/material/Link";
+import { Icon } from "@mui/material";
+import { utils } from "../../utils";
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: 70,//`calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: 70, //`calc(${theme.spacing(8)} + 1px)`,
-    },
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: 70, //`calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: 70, //`calc(${theme.spacing(8)} + 1px)`,
+  },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
+  }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
-export default function SideBar({element}) {
-    const [open, setOpen] = React.useState(false);
+export default function SideBar({ element }) {
+  const [open, setOpen] = React.useState(false);
 
-    const updateDrawerOpenClose = () => {
-        setOpen(!open);
-    }
+  const updateDrawerOpenClose = () => {
+    setOpen(!open);
+  };
 
-    return  (
-        <div>
-            <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="fixed" open={open}>
-                <Toolbar style={{background: "linear-gradient(to right, #4cdbff, #19ebd2)"}}>
-                    <Icon color="inherit" aria-label="open drawer" edge="start" 
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar style={{ backgroundColor: "white" }}>
+            <Icon
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                color: "#757575",
+                marginRight: 5,
+                ...(open && { display: "none" }),
+              }}
+            >
+              <LocalGroceryStoreIcon />
+            </Icon>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              style={{ color: "black" }}
+            >
+              Store Flex
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <button
+            onClick={updateDrawerOpenClose}
+            style={{
+              margin: `5% 0% 0% ${open ? "13.9%" : "3.4%"}`,
+              position: "fixed",
+              width: "30px",
+              padding: "0",
+              height: "30px",
+            }}
+          >
+            {open ? (
+              <NavigateBeforeIcon sx={{ marginTop: "7%" }} />
+            ) : (
+              <NavigateNextIcon sx={{ marginTop: "7%" }} />
+            )}
+          </button>
+          <DrawerHeader style={{ justifyContent: "start" }}>
+            <IconButton>
+              <LocalGroceryStoreIcon />
+            </IconButton>
+            <span>Store Flex</span>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {utils.menuRoutes.map((ele) => {
+              const { text, MenuItemIcon, url } = ele;
+              return (
+                <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                  <ListItemButton
                     sx={{
-                            marginRight: 5,
-                            ...(open && { display: 'none' }),
-                        }}
+                      minHeight: 48,
+                      justifyContent: open ? "initial" : "center",
+                      px: 2.5,
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : "auto",
+                        justifyContent: "center",
+                        display: open ? "flex" : "block",
+                      }}
                     >
-                        <LocalGroceryStoreIcon />
-                    </Icon>
-                    <Typography variant="h6" noWrap component="div">
-                        Mini variant drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <button onClick={updateDrawerOpenClose} style={{margin: `5% 0% 0% ${open ? '13.9%' : "3.4%"}`, position: "fixed", width: "30px", padding: "0", height: "30px"}}>{open ? <NavigateBeforeIcon sx={{marginTop: "7%"}} /> : <NavigateNextIcon sx={{marginTop: "7%"}} />}</button>
-                <DrawerHeader style={{justifyContent: "start"}}>
-                    <IconButton>
-                        <LocalGroceryStoreIcon />
-                    </IconButton>
-                    <span>Store Flex</span>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                        display: open ? 'flex' : 'block'
-                                    }}
-                                >
-                                    <div>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</div>
-                                    {!open && <div style={{fontSize: 'xx-small'}}>{text}</div>}
-                                </ListItemIcon>
-                                <ListItemText primary={text} sx={{opacity : open ? 1 : 0}}/>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                            <ListItemButton
-                                sx={{
-                                    minHeight: 48,
-                                    justifyContent: open ? 'initial' : 'center',
-                                    px: 2.5,
-                                }}
-                            >
-                                <ListItemIcon
-                                    sx={{
-                                        minWidth: 0,
-                                        mr: open ? 3 : 'auto',
-                                        justifyContent: 'center',
-                                    }}
-                                >
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <DrawerHeader />
-                {element()}
-            </Box>
+                      <Link href={url} underline="none">
+                        {MenuItemIcon}
+                        {open && (
+                          <span style={{ fontSize: "xx-small" }}>{text}</span>
+                        )}
+                      </Link>
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          {element()}
         </Box>
-        </div>
-    )
+      </Box>
+    </div>
+  );
 }
---
+++ frontend/src/utils/index.jsx
@@ -0,0 +1,9 @@
+// import HomeIcon from "@mui/icons-material/Home";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+
+export const utils = {
+  menuRoutes: [
+    // { text: "Home", MenuItemIcon: <HomeIcon />, url: "/home" },
+    { text: "Stores", MenuItemIcon: <StorefrontIcon />, url: "/stores" },
+  ],
+};
